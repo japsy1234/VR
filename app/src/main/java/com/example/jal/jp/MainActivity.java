0626bb6082732d99b1fe567dc2b5549cd42f19ac
japsy123 <jal08panchal@gmail.com>
2017-06-21 02:43:08 -0700

    Awesome Commit

---
+++ app/src/main/java/com/example/jal/jp/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.jal.jp;
+
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private VrPanoramaView mVrPanoramaView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mVrPanoramaView = (VrPanoramaView) findViewById(R.id.pano_view);
+
+        loadPhotoSphere();
+
+    }
+    private void loadPhotoSphere() {
+        //This could take a while. Should do on a background thread, but fine for current example
+        VrPanoramaView.Options options = new VrPanoramaView.Options();
+        InputStream inputStream = null;
+
+        AssetManager assetManager = getAssets();
+
+        try {
+            inputStream = assetManager.open("jal.jpg");
+            options.inputType = VrPanoramaView.Options.TYPE_MONO;
+            mVrPanoramaView.loadImageFromBitmap(BitmapFactory.decodeStream(inputStream), options);
+            inputStream.close();
+        } catch (IOException e) {
+            Log.e("Tuts+", "Exception in loadPhotoSphere: " + e.getMessage());
+        }
+    }
+    protected void onPause() {
+        mVrPanoramaView.pauseRendering();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mVrPanoramaView.resumeRendering();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mVrPanoramaView.shutdown();
+        super.onDestroy();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
