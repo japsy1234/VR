c50041164a0a1fbda3fd201b6e0f0eef088ec355
japsy123 <jal08panchal@gmail.com>
2017-06-21 22:07:49 -0700

    Awesome Commit

--- app/src/main/java/com/example/jal/jp/MainActivity.java
+++ app/src/main/java/com/example/jal/jp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jal.jp;
 
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 
@@ -18,49 +20,28 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private VrPanoramaView mVrPanoramaView;
+
+    public Button first_button;
+
+    public void init(){
+        first_button = (Button)findViewById(R.id.explore);
+        first_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explore = new Intent(MainActivity.this,Choices.class);
+                startActivity(explore);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mVrPanoramaView = (VrPanoramaView) findViewById(R.id.pano_view);
-
-        loadPhotoSphere();
+        init();
 
     }
-    private void loadPhotoSphere() {
-        //This could take a while. Should do on a background thread, but fine for current example
-        VrPanoramaView.Options options = new VrPanoramaView.Options();
-        InputStream inputStream = null;
 
-        AssetManager assetManager = getAssets();
-
-        try {
-            inputStream = assetManager.open("jal.jpg");
-            options.inputType = VrPanoramaView.Options.TYPE_MONO;
-            mVrPanoramaView.loadImageFromBitmap(BitmapFactory.decodeStream(inputStream), options);
-            inputStream.close();
-        } catch (IOException e) {
-            Log.e("Tuts+", "Exception in loadPhotoSphere: " + e.getMessage());
-        }
-    }
-    protected void onPause() {
-        mVrPanoramaView.pauseRendering();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mVrPanoramaView.resumeRendering();
-    }
-
-    @Override
-    protected void onDestroy() {
-        mVrPanoramaView.shutdown();
-        super.onDestroy();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -83,3 +64,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
