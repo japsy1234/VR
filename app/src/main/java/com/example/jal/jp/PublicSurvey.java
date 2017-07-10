6610a3fcf2968e24988db920ab3c60708cc63f00
japsy123 <jal08panchal@gmail.com>
2017-07-09 23:21:47 -0700

    Aditya3 Commit

---
+++ app/src/main/java/com/example/jal/jp/PublicSurvey.java
@@ -0,0 +1,17 @@
+package com.example.jal.jp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class PublicSurvey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_survey);
+
+
+    }
+}
